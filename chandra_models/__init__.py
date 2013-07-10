654eff027a0a57f3617c76f65e4bada2d357b7d4
Matthew Dahmer <matthew.dahmer@gmail.com>
2013-07-10 15:16:20 -0400

    Make consistent with updated SOT version
    Signed-off-by: Matthew Dahmer <matthew.dahmer@gmail.com>

--- chandra_models/__init__.py
+++
@@ -1,4 +0,0 @@
-import os
-from .get_model_spec import *
-
-__version__ = open(os.path.join(os.path.dirname(__file__),'VERSION')).read().strip()
---
+++ chandra_models/chandra_models/__init__.py
@@ -0,0 +1,2 @@
+from .get_model_spec import *
+from .version import __version__
